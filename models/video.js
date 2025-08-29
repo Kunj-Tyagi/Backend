de317d44fd655bbb97b09d43d2fd95e846ab1ad3
Kunj Tyagi <kunjtyagi24@gmail.com>
2025-08-30 00:19:19 +0530

    first commit

--- models/video.js
+++ models/video.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-//   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   videoUrl: { type: String, required: true },   // S3 link
-  // analysisResult: { type: Object },             // Future AI output
-  // uploadedAt: { type: Date, default: Date.now },
+  analysisResult: { type: Object },             // Future AI output
+  uploadedAt: { type: Date, default: Date.now },
 });
 
 export default mongoose.model("Video", videoSchema);
